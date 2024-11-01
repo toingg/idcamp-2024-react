b08b877b5ca543e453c430ed4b45064b4d911f82
Fachrizal Ardiansyah <fach.ardiansyah@gmail.com>
2024-11-01 21:23:49 +0700

    Finished modul: Fucntional Programming

---
+++ Belajar Dasar Pemrograman JavaScript/Kuis Coding - Functional Programming.js
@@ -0,0 +1,54 @@
+const sampleProducts = [
+  { id: 1, name: "Laptop", category: "Electronics", price: 1000 },
+  { id: 2, name: "Phone", category: "Electronics", price: 500 },
+  { id: 3, name: "Shirt", category: "Apparel", price: 50 },
+  { id: 4, name: "Shoes", category: "Apparel", price: 80 },
+  { id: 5, name: "Watch", category: "Accessories", price: 200 },
+];
+
+function getProductsByCategory(products, category) {
+  /**
+   * TODO:
+   * Gunakan metode array immutable untuk mengembalikan array produk yang termasuk dalam kategori yang diberikan.
+   */
+  const newArray = products.filter((product) => product.category === category);
+
+  return newArray;
+}
+
+function findProductById(products, id) {
+  /**
+   * TODO:
+   * Gunakan metode array immutable untuk mengembalikan produk dengan ID yang cocok.
+   */
+  const newArray = products.find((product) => product.id === id);
+  return newArray;
+}
+
+function calculateTotalPrice(products) {
+  /**
+   * TODO:
+   * Gunakan metode array immutable untuk menghitung total harga semua produk.
+   */
+  const newArray = products.reduce((acc, product) => acc + product.price, 0);
+  return newArray;
+}
+
+function applyDiscount(products, discount) {
+  /**
+   * TODO:
+   * Gunakan metode array immutable untuk mengembalikan array baru,
+   * di mana setiap produk memiliki harga yang sudah dikurangi dengan diskon yang diberikan.
+   */
+  const newArray = products.map((product) => ({
+    ...product,
+    price: product.price - (product.price * discount) / 100,
+  }));
+
+  return newArray;
+}
+
+console.log(getProductsByCategory(sampleProducts, "Electronics")); // Should return products with id 1 and 2
+console.log(calculateTotalPrice(sampleProducts)); // Should return 1830
+console.log(applyDiscount(sampleProducts, 10)); // Should return products with prices reduced by 10%
+console.log(findProductById(sampleProducts, 3)); // Should return the product with id 3
